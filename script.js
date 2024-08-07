f092c92ebb704ca83372308f09b288b157638cb2
Tourad <tourad959@gmail.com>
2024-08-07 17:36:23 +0000

    Single Round Play function added

--- script.js
+++ script.js
@@ -18,7 +18,55 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let userInput = prompt("Enter your choice: ");
-    return userInput;
+    if (userInput.toLowerCase() === 'rock') {
+        return userInput;
+    } else if (userInput.toLowerCase() === 'paper') {
+        return userInput;
+    } else if (userInput.toLowerCase() === 'scissors') {
+        return userInput;
+    } else {
+        return 'Invalid choice';
+    }
 }
 
-console.log(getHumanChoice());
+// Keeping track of the players score
+
+let humanScore = 0;
+let computerScore = 0;
+
+// Single Round Play
+
+function playRound(humanChoice, computerChoice) {
+
+    if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else if (humanChoice === computerChoice) {
+        return 'Tie.';
+    } else if ((computerChoice === 'rock' && humanChoice === 'scissors') || (computerChoice === 'paper' && humanChoice === 'rock') || (computerChoice === 'scissors' && humanChoice === 'paper')) {
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}`;
+    } else {
+        return 'Invalid Choice!';
+    }
+}
+
+// Play 5 Rounds
+
+let humanSelection;
+let computerSelection;
+
+function playGame() {
+    for(let i = 0; i < 5; i++) {
+        humanSelection = getHumanChoice();
+        computerSelection = getComputerChoice();
+        console.log(playRound(humanSelection, computerSelection));
+    }
+    if (computerScore > humanScore) {
+        console.log('Winner: Computer.');
+    } else {
+        console.log('Winner: You.');
+    }
+}
+
+playGame();
